87a227e24064d48d8feab03370b4fb0fce9f67d3
ruchi06 <ruchi06@gmail.com>
2022-12-17 16:30:46 +0530

    CheackAnagrams

---
+++ AlgorithmProgram/AlgorithmProgram/Anagram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProgram
+{
+    public class Anagram
+    {
+        public void CheckAnagram(string str1, string str2)
+        {
+            char[] ch1, ch2;
+            string val1, val2;
+            ch1 = str1.ToLower().ToCharArray();
+            ch2 = str2.ToLower().ToCharArray();
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+            val1 = new string(ch1);
+            val2 = new string(ch2);
+
+            if (val1 == val2)
+            {
+                Console.WriteLine("Both the words are Anagrams");
+
+            }
+            else
+            {
+                Console.WriteLine("Both the Words are not Anagrams");
+            }
+        }
+    }
+}
---
+++ AlgorithmProgram/AlgorithmProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AlgorithmProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Anagram input = new Anagram();
+            input.CheckAnagram("RUCHA", "AHCUR");
+        }
+    }
+}
